0b3db89d73eb65e459634ca12250ab3b8990b6a5
Theodore Schnepper <ayiga@msn.com>
2017-07-14 00:10:32 -0600

    maintainence
    remove channel based functions
    
    add comments to every relevant method and struct

--- image/rgba.go
+++ image/rgba.go
@@ -1,13 +1,18 @@
 package image
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 
 	giftcolor "github.com/disintegration/gift/image/color"
 )
 
+// The RGBA images represented within this package have all been modeled after the
+// code within the image package. Most of it is copied and modified for the Floating
+// Point color models.
+
+// F32RGBA represents an RGBA image whose channels are represented
+// by float32s.
 type F32RGBA struct {
 	// Pix holds the image's pixles, in R, G, B, A order. The pixel at
 	// (x, y) stars at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*4].
@@ -35,6 +40,9 @@ func (i *F32RGBA) At(x, y int) color.Color {
 	return i.F32RGBAAt(x, y)
 }
 
+// F32RGBAAt returns the underlying F32RBA color at the given coordinates.
+// If the x and y values are out of range, it will return an empty
+// F32RGBA color instead
 func (i *F32RGBA) F32RGBAAt(x, y int) giftcolor.F32RGBA {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return giftcolor.F32RGBA{}
@@ -43,17 +51,21 @@ func (i *F32RGBA) F32RGBAAt(x, y int) giftcolor.F32RGBA {
 	idx := i.PixOffset(x, y)
 
 	return giftcolor.F32RGBA{
-		i.Pix[idx+0],
-		i.Pix[idx+1],
-		i.Pix[idx+2],
-		i.Pix[idx+3],
+		R: i.Pix[idx+0],
+		G: i.Pix[idx+1],
+		B: i.Pix[idx+2],
+		A: i.Pix[idx+3],
 	}
 }
 
+// PixOffset returns the Pix index offset for the Red color channel in the
+// Pix slice
 func (i *F32RGBA) PixOffset(x, y int) int {
 	return (y-i.Rect.Min.Y)*i.Stride + (x-i.Rect.Min.X)*4
 }
 
+// Set will set the the given color, c, at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothing will happen.
 func (i *F32RGBA) Set(x, y int, c color.Color) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -67,6 +79,8 @@ func (i *F32RGBA) Set(x, y int, c color.Color) {
 	i.Pix[idx+3] = c1.A
 }
 
+// SetF32RGBA will set the given F32RGBA color at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothign will happen.
 func (i *F32RGBA) SetF32RGBA(x, y int, c giftcolor.F32RGBA) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -123,6 +137,8 @@ func NewF32RGBA(r image.Rectangle) *F32RGBA {
 	return &F32RGBA{pix, 4 * w, r}
 }
 
+// F64RGBA represents an RGBA image whose channels are represented
+// by float64s.
 type F64RGBA struct {
 	// Pix holds the image's pixles, in R, G, B, A order. The pixel at
 	// (x, y) stars at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*4].
@@ -150,6 +166,9 @@ func (i *F64RGBA) At(x, y int) color.Color {
 	return i.F64RGBAAt(x, y)
 }
 
+// F64RGBAAt returns the underlying F32RBA color at the given coordinates.
+// If the x and y values are out of range, it will return an empty
+// F64RGBA color instead
 func (i *F64RGBA) F64RGBAAt(x, y int) giftcolor.F64RGBA {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return giftcolor.F64RGBA{}
@@ -158,17 +177,21 @@ func (i *F64RGBA) F64RGBAAt(x, y int) giftcolor.F64RGBA {
 	idx := i.PixOffset(x, y)
 
 	return giftcolor.F64RGBA{
-		i.Pix[idx+0],
-		i.Pix[idx+1],
-		i.Pix[idx+2],
-		i.Pix[idx+3],
+		R: i.Pix[idx+0],
+		G: i.Pix[idx+1],
+		B: i.Pix[idx+2],
+		A: i.Pix[idx+3],
 	}
 }
 
+// PixOffset returns the Pix index offset for the Red color channel in the
+// Pix slice
 func (i *F64RGBA) PixOffset(x, y int) int {
 	return (y-i.Rect.Min.Y)*i.Stride + (x-i.Rect.Min.X)*4
 }
 
+// Set will set the the given color, c, at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothing will happen.
 func (i *F64RGBA) Set(x, y int, c color.Color) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -182,6 +205,8 @@ func (i *F64RGBA) Set(x, y int, c color.Color) {
 	i.Pix[idx+3] = c1.A
 }
 
+// SetF64RGBA will set the given F64RGBA color at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothign will happen.
 func (i *F64RGBA) SetF64RGBA(x, y int, c giftcolor.F64RGBA) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -238,6 +263,8 @@ func NewF64RGBA(r image.Rectangle) *F64RGBA {
 	return &F64RGBA{pix, 4 * w, r}
 }
 
+// C64RGBA represents an RGBA image whose channels are represented
+// by complex64s.
 type C64RGBA struct {
 	// Pix holds the image's pixles, in R, G, B, A order. The pixel at
 	// (x, y) stars at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*4].
@@ -265,6 +292,9 @@ func (i *C64RGBA) At(x, y int) color.Color {
 	return i.C64RGBAAt(x, y)
 }
 
+// C64RGBAAt returns the underlying F32RBA color at the given coordinates.
+// If the x and y values are out of range, it will return an empty
+// C64RGBA color instead
 func (i *C64RGBA) C64RGBAAt(x, y int) giftcolor.C64RGBA {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return giftcolor.C64RGBA{}
@@ -280,10 +310,14 @@ func (i *C64RGBA) C64RGBAAt(x, y int) giftcolor.C64RGBA {
 	}
 }
 
+// PixOffset returns the Pix index offset for the Red color channel in the
+// Pix slice
 func (i *C64RGBA) PixOffset(x, y int) int {
 	return (y-i.Rect.Min.Y)*i.Stride + (x-i.Rect.Min.X)*4
 }
 
+// Set will set the the given color, c, at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothing will happen.
 func (i *C64RGBA) Set(x, y int, c color.Color) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -297,6 +331,8 @@ func (i *C64RGBA) Set(x, y int, c color.Color) {
 	i.Pix[idx+3] = c1.A
 }
 
+// SetC64RGBA will set the given C64RGBA color at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothign will happen.
 func (i *C64RGBA) SetC64RGBA(x, y int, c giftcolor.C64RGBA) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -353,6 +389,8 @@ func NewC64RGBA(r image.Rectangle) *C64RGBA {
 	return &C64RGBA{pix, 4 * w, r}
 }
 
+// C128RGBA represents an RGBA image whose channels are represented
+// by complex128s.
 type C128RGBA struct {
 	// Pix holds the image's pixles, in R, G, B, A order. The pixel at
 	// (x, y) stars at Pix[(y-Rect.Min.Y)*Stride + (x-Rect.Min.X)*4].
@@ -380,6 +418,9 @@ func (i *C128RGBA) At(x, y int) color.Color {
 	return i.C128RGBAAt(x, y)
 }
 
+// C128RGBAAt returns the underlying F32RBA color at the given coordinates.
+// If the x and y values are out of range, it will return an empty
+// C128RGBA color instead
 func (i *C128RGBA) C128RGBAAt(x, y int) giftcolor.C128RGBA {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return giftcolor.C128RGBA{}
@@ -395,10 +436,14 @@ func (i *C128RGBA) C128RGBAAt(x, y int) giftcolor.C128RGBA {
 	}
 }
 
+// PixOffset returns the Pix index offset for the Red color channel in the
+// Pix slice
 func (i *C128RGBA) PixOffset(x, y int) int {
 	return (y-i.Rect.Min.Y)*i.Stride + (x-i.Rect.Min.X)*4
 }
 
+// Set will set the the given color, c, at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothing will happen.
 func (i *C128RGBA) Set(x, y int, c color.Color) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -412,6 +457,8 @@ func (i *C128RGBA) Set(x, y int, c color.Color) {
 	i.Pix[idx+3] = c1.A
 }
 
+// SetC128RGBA will set the given C128RGBA color at the given coordinates, x and y.
+// If the given coordinates are not within bounds, then nothign will happen.
 func (i *C128RGBA) SetC128RGBA(x, y int, c giftcolor.C128RGBA) {
 	if !(image.Point{x, y}.In(i.Rect)) {
 		return
@@ -461,62 +508,9 @@ func (i *C128RGBA) Opaque() bool {
 	return true
 }
 
-func (i *C128RGBA) getChannel(c int) [][]complex128 {
-	w, h := i.Rect.Dx(), i.Rect.Dy()
-	r := make([][]complex128, h)
-	i0, i1 := c, i.Rect.Dx()*4
-	for y, yi := i.Rect.Min.Y, 0; y < i.Rect.Max.Y; y, yi = y+1, yi+1 {
-		r[yi] = make([]complex128, w)
-		for idx, ii := i0, 0; idx < i1; idx, ii = idx+4, ii+1 {
-			r[yi][ii] = i.Pix[idx]
-		}
-		i0 += i.Stride
-		i1 += i.Stride
-	}
-
-	return r
-}
-
-func (i *C128RGBA) RedChannel() [][]complex128 {
-	return i.getChannel(0)
-}
-
-func (i *C128RGBA) GreenChannel() [][]complex128 {
-	return i.getChannel(1)
-}
-
-func (i *C128RGBA) BlueChannel() [][]complex128 {
-	return i.getChannel(2)
-}
-
-func (i *C128RGBA) AlphaChannel() [][]complex128 {
-	return i.getChannel(3)
-}
-
 // NewC128RGBA returns a new C128RGBA image with the given bounds.
 func NewC128RGBA(r image.Rectangle) *C128RGBA {
 	w, h := r.Dx(), r.Dy()
 	pix := make([]complex128, 4*w*h)
 	return &C128RGBA{pix, 4 * w, r}
-}
-
-func NewC128RGBAFromChannels(r, g, b, a [][]complex128) *C128RGBA {
-	h := len(r)
-	w := len(r[0])
-
-	fmt.Printf("image dimensions: %d x %d\n", w, h)
-
-	i := NewC128RGBA(image.Rect(0, 0, w, h))
-
-	for y := i.Rect.Min.Y; y < i.Rect.Max.Y; y++ {
-		for x := i.Rect.Min.X; x < i.Rect.Max.X; x++ {
-			idx := i.PixOffset(x, y)
-			i.Pix[idx+0] = r[y][x]
-			i.Pix[idx+1] = g[y][x]
-			i.Pix[idx+2] = b[y][x]
-			i.Pix[idx+3] = a[y][x]
-		}
-	}
-
-	return i
 }
